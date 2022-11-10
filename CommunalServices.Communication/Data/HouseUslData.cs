2ffed07d81a796d510cac8bc915308fd70f94e75
Svitkin <81796314+RIP-webmaster@users.noreply.github.com>
2022-11-10 15:23:04 +0500

    Add HouseUslData

---
+++ CommunalServices.Communication/Data/HouseUslData.cs
@@ -0,0 +1,27 @@
+﻿/* Communal services system integration 
+ * Copyright (c) 2022,  Svitkin V.G. 
+ * License: BSD 2.0 */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CommunalServices.Communication.Data
+{
+    public class HouseUslData
+    {
+        public string HouseGUID { get; set; }
+        public decimal HouseArea { get; set; }
+        public string HouseAddress { get; set; }
+        public string UslName { get; set; }
+        public decimal UslTarif { get; set; }
+
+        public static decimal ParseArea(string areaStr)
+        {
+            areaStr = areaStr.Replace('.',',');
+            CultureInfo ci = new CultureInfo(CultureInfo.CurrentCulture.Name);
+            ci.NumberFormat.NumberDecimalSeparator = ",";
+            return decimal.Parse(areaStr, ci);
+        }
+    }
+}
